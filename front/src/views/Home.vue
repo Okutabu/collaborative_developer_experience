1f188d13132abdbd47495ed7100a1b26ae8a3055
aubrypau <92160857+aubrypau@users.noreply.github.com>
2023-02-18 22:18:30 +0100

    Revert "Link front and back"

---
+++ front/src/views/Home.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { storeToRefs } from 'pinia';
+
+import { useAuthStore } from '@/stores';
+
+const authStore = useAuthStore();
+const { user } = storeToRefs(authStore);
+</script>
+
+<template>
+    <div v-if="user">
+        <h1>Bonjour {{user.firstName}} {{user.lastName}} !</h1>
+        <p> Votre id Stack overflow est : {{ user.idSTOW }}</p>
+        <p><router-link to="/users">Manage Users</router-link></p>
+    </div>
+</template>
