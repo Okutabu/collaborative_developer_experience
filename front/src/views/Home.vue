4aec5db657e7021f680680efab512a6cca85096e
AlexOS <82172155+alexandredesbos@users.noreply.github.com>
2023-02-20 15:45:29 +0100

    debug landing page

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -5,12 +5,13 @@ import { useAuthStore } from '@/stores';
 
 const authStore = useAuthStore();
 const { user } = storeToRefs(authStore);
+
 </script>
 
 <template>
     <div v-if="user">
-        <h1>Bonjour {{user.firstName}} !</h1>
-        <p> Votre id Stack overflow est : {{ user.idSTOW }}</p>
+        <h1>Bonjour {{user.user.surname}} !</h1>
+        <p> Votre id Stack overflow est : {{ user.user.idSTOW }}</p>
         <p><router-link to="/users">Manage Users</router-link></p>
     </div>
 </template>
