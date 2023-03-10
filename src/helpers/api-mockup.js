6ad594f1e1ddf1984fe1671662c66128b7faa2c3
Okutabu <67596669+Okutabu@users.noreply.github.com>
2023-03-10 18:38:48 +0100

    Update api-mockup.js

--- src/helpers/api-mockup.js
+++ src/helpers/api-mockup.js
@@ -6,29 +6,6 @@ let users = JSON.parse(localStorage.getItem(usersKey)) || [];
 
 export function loadMirageInDev() {
 
-  if (process.env.NODE_ENV === "development") {
-    loadMirage().then(({ Server }) => {
-      return new Server({
-        routes() {
-          this.namespace = "http://mockapi:1234";
-
-          this.get("/admin/statistics", () => {
-            return [{
-				answer: "Statistics found",
-				nbTags: 999,
-				nbUsers: 888,
-				topTags: ["tag1", "tag2", "tag3", "tag4", "tag5"],
-				error: 0
-			}];
-          });
-          
-        },
-      });
-    });
-  }
-  
-  
-
         if (import.meta.env.MODE === "development") {
             loadMirage().then(({ Server }) => {
                 return new Server({
