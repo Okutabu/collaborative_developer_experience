b6368adab3e7d9a0482fd665c6b570cf84983f7b
aubrypau <paul.aubry.ent@gmail.com>
2023-03-13 12:41:47 +0100

    location fichier

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const similarity = require('./similarityQueries');
+const similarity = require('../similarityQueries');
 
 /* GET users listing. */
 router.get('', function(req, res, next) {
