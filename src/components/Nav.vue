a0f14d2a61c6d36f86342aef14e4731eb642b5c8
Okutabu <67596669+Okutabu@users.noreply.github.com>
2023-03-30 18:05:54 +0200

    Visual appeal improved

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,127 +1,207 @@
 <script setup>
+import { ref } from 'vue'
+import logoURL from '../assets/ptc.png'
 import { useAuthStore } from '@/stores';
 
 const authStore = useAuthStore();
+
+const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
+
+const ToggleMenu = () => {
+	is_expanded.value = !is_expanded.value
+	localStorage.setItem("is_expanded", is_expanded.value)
+}
 </script>
 
 <template>
-    <!-- if the url start with /admin render the div bellow -->
     <div v-if="$route.path.startsWith('/admin')">
-        <nav v-show="authStore.user" class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="navbar-brand">
-                <img src="../assets/Logo-Positive-Thinking-Company.png" alt="logo" width="300" height="200">
+        <aside :class="`${is_expanded ? 'is-expanded' : ''}`" v-show="authStore.user">
+            <div class="logo">
+                <img :src="logoURL" alt="Vue" /> 
             </div>
-            <div class="container-fluid">
-                <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav flex-column">
-                    <li class="nav-item">
-                        <RouterLink to="/admin/list" class="nav-item nav-link"><i class="fa fa-user"></i>&nbsp; Home</RouterLink>
-                    </li>
-                    <li class="nav-item">
-                        <RouterLink to="/admin/stats"    class="nav-item nav-link"><i class="fa fa-unlock"></i> &nbsp; statistiques</RouterLink>
-                    </li>
-                    <li class="nav-item">
-                        <RouterLink to="/admin/collecteur" class="nav-item nav-link"><i class="fa fa-unlock"></i> &nbsp; Collecteur</RouterLink>
-                    </li>
-                </ul>
-                </div>
+
+            <div class="menu-toggle-wrap">
+                <button class="menu-toggle" @click="ToggleMenu">
+                    <span class="material-icons">keyboard_double_arrow_right</span>
+                </button>
             </div>
-        </nav>
+
+            <h3>Menu</h3>
+            <div class="menu">
+                <RouterLink to="/admin/list" class="button">
+                    <span class="material-icons">home</span>
+                    <span class="text">Home</span>
+                </RouterLink>
+                <RouterLink to="/admin/stats" class="button">
+                    <span class="material-icons">insights</span>
+                    <span class="text">Statistiques</span>
+                </RouterLink>
+                <RouterLink to="/admin/collecteur" class="button">
+                    <span class="material-icons">smart_toy</span>
+                    <span class="text">Collecteur</span>
+                </RouterLink>
+            </div>
+
+            <div class="flex"></div>
+            
+            <div class="menu" @click="authStore.logout()">
+                <RouterLink to="/logout" class="button" >
+                    <span class="material-icons">logout</span>
+                    <span class="text">Logout</span>
+                </RouterLink>
+            </div>
+        </aside>
     </div>
     <div v-else>
-        <nav v-show="authStore.user" class="navbar navbar-expand-lg navbar-light bg-light" >
-            <div class="navbar-brand">
-                <img src="../assets/Logo-Positive-Thinking-Company.png" alt="logo" width="300" height="200">
+        <aside :class="`${is_expanded ? 'is-expanded' : ''}`" v-show="authStore.user">
+            <div class="logo">
+                <img :src="logoURL" alt="Vue" /> 
             </div>
-            <div class="container-fluid">
-                <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav flex-column">
-                    <li class="nav-item">
-                        <RouterLink to="/overview" class="nav-item nav-link"><i class="fa fa-user"></i>&nbsp; Home</RouterLink>
-                    </li>
-                    <li class="nav-item">
-                        <RouterLink to="/statistics" class="nav-item nav-link"><i class="fa fa-unlock"></i> &nbsp; Mes statistiques</RouterLink>
-                    </li>
-                    <!-- <li class="nav-item">
-                        <RouterLink to="/admin" class="nav-item nav-link"><i class="fa fa-unlock"></i> &nbsp; Admin</RouterLink>
-                    </li> -->
-                    <!-- <li class="nav-item">
-                        <RouterLink to="/message" class="nav-item nav-link"><i class="fa fa-solid fa-envelope"></i>&nbsp; Message</RouterLink>
-                    </li> -->
-                    <li class="nav-item">
-                        <button @click="authStore.logout()" class="btn btn-link nav-item nav-link"><i class="fa fa-sign-out" style="font-size:24px"></i> Logout</button>
-                    </li>
-                </ul>
-                </div>
+
+            <div class="menu-toggle-wrap">
+                <button class="menu-toggle" @click="ToggleMenu">
+                    <span class="material-icons">keyboard_double_arrow_right</span>
+                </button>
             </div>
-        </nav>
+
+            <h3>Menu</h3>
+            <div class="menu">
+                <RouterLink to="/overview" class="button">
+                    <span class="material-icons">home</span>
+                    <span class="text">Home</span>
+                </RouterLink>
+                <RouterLink to="/statistics" class="button">
+                    <span class="material-icons">insights</span>
+                    <span class="text">Statistics</span>
+                </RouterLink>
+            </div>
+
+            <div class="flex"></div>
+            
+            <div class="menu" @click="authStore.logout()">
+                <RouterLink to="/logout" class="button" >
+                    <span class="material-icons">logout</span>
+                    <span class="text">Logout</span>
+                </RouterLink>
+            </div>
+        </aside>
     </div>
-
-
 </template>
 
+
 <style scoped>
-
-.navbar{
-    background-color: #ffffff;
-    width: 300px;
-    display: flex;
-    flex-direction: column;
-}
-.navbar-nav {
-  width: 100%;
-}
-
-.navbar-nav li{
-    margin-bottom: 30px;
-}
-
-.nav-link {
-  color: #000000;
+aside {
+  display: flex;
+  flex-direction: column;
+  background-color: var(--dark);
+  color: var(--light);
+  width: calc(2rem + 32px);
+  overflow: hidden;
+  min-height: 100vh;
   padding: 1rem;
-  font-size: 1.2rem;
-  text-align: center;
-  width: 100%;
-  border-radius: 25px;
+  transition: 0.2s ease-in-out;
 }
-
-.navbar .nav-link.active {
-  background-color: #25b3c3;
-  color: #ffffff;
+aside .flex {
+  flex: 0.9 0.9 0%;
 }
-
-.nav-link:hover {
-  background-color: #e6e6e6;
-
+aside .logo {
+  margin-bottom: 1rem;
 }
-.nav-item{
-    animation: reveal 1s cubic-bezier(.17,.67,.4,1.02);
+aside .logo img {
+  width: 2rem;
 }
-.navbar-brand {
-    margin-bottom: 100px;
-    animation: reveal 1s ease-in-out;
+aside .menu-toggle-wrap {
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 1rem;
+  position: relative;
+  top: 0;
+  transition: 0.2s ease-in-out;
 }
-
-@keyframes reveal{
-    0%{
-        opacity: 0;
-        transform: translateY(100px);
-    }
-    100%{
-        opacity: 1;
-        transform: translateY(0px);
-    }
+aside .menu-toggle-wrap .menu-toggle {
+  transition: 0.2s ease-in-out;
 }
-
-@keyframes fade-in {
-    0% {
-        opacity: 0;
-    }
-    100% {
-        opacity: 1;
-    }
+aside .menu-toggle-wrap .menu-toggle .material-icons {
+  font-size: 2rem;
+  color: var(--light);
+  transition: 0.2s ease-out;
 }
-
+aside .menu-toggle-wrap .menu-toggle:hover .material-icons {
+  color: var(--primary);
+  transform: translateX(0.5rem);
+}
+aside h3, aside .button .text {
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+}
+aside h3 {
+  color: var(--grey);
+  font-size: 0.875rem;
+  margin-bottom: 0.5rem;
+  text-transform: uppercase;
+}
+aside .menu {
+  margin: 0 -1rem;
+}
+aside .menu .button {
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  transition: 0.2s ease-in-out;
+  padding: 0.5rem 1rem;
+}
+aside .menu .button .material-icons {
+  font-size: 2rem;
+  color: var(--light);
+  transition: 0.2s ease-in-out;
+}
+aside .menu .button .text {
+  color: var(--light);
+  transition: 0.2s ease-in-out;
+}
+aside .menu .button:hover {
+  background-color: var(--dark-alt);
+}
+aside .menu .button:hover .material-icons, aside .menu .button:hover .text {
+  color: var(--primary);
+}
+aside .menu .button.active {
+  background-color: var(--dark-alt);
+  border-right: 5px solid var(--primary);
+}
+aside .menu .button.router-link-exact-active .material-icons, aside .menu .button.router-link-exact-active .text {
+  color: var(--primary);
+}
+aside .footer {
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+}
+aside .footer p {
+  font-size: 0.875rem;
+  color: var(--grey);
+}
+aside.is-expanded {
+  width: var(--sidebar-width);
+}
+aside.is-expanded .menu-toggle-wrap {
+  top: -3rem;
+}
+aside.is-expanded .menu-toggle-wrap .menu-toggle {
+  transform: rotate(-180deg);
+}
+aside.is-expanded h3, aside.is-expanded .button .text {
+  opacity: 1;
+}
+aside.is-expanded .button .material-icons {
+  margin-right: 1rem;
+}
+aside.is-expanded .footer {
+  opacity: 0;
+}
+@media (max-width: 1024px) {
+  aside {
+    position: absolute;
+    z-index: 99;
+  }
+}
 </style>
-    
-        
