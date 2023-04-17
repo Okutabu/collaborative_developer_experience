d65e8b47b75a71fb8d81806662e4de78cf65eef6
MouxyMan <lologan789@gmail.com>
2023-04-17 17:22:50 +0200

    arrows font awesome avancement

--- front/src/views/admin/Profile.vue
+++ front/src/views/admin/Profile.vue
@@ -17,10 +17,13 @@ profileStore.setUserProfile(route.params.id);
 
 <template>
 
+    <font-awesome-icon icon="fa-solid fa-arrow-left" />
+
     <div class="container-profile-">
         <div class="container-card"> <UserCard2 :nom=userCardValue.userProfile[0].pseudo :techno=userCardValue.userProfile[1] :avatar=userCardValue.userProfile[0].avatar  :reco=userCardValue.userProfile[1][0].techno :key=userCardValue :lastInteract=userCardValue.userProfile[0].lastInteraction /> </div>
     </div>
 
+    <font-awesome-icon icon="fa-solid fa-arrow-right" />
 
 </template>
 
@@ -32,6 +35,8 @@ profileStore.setUserProfile(route.params.id);
     background-color: #f5f5f5;
 }
 
-
+.fa {
+  cursor: pointer;
+}
 
 </style>
