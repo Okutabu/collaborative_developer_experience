205663b94e4f123c7d3e5283ed8b2e3892b27d37
AlexOS <82172155+alexandredesbos@users.noreply.github.com>
2023-04-27 21:43:24 +0200

    Update MarketingHome.vue

--- front/src/views/marketing/MarketingHome.vue
+++ front/src/views/marketing/MarketingHome.vue
@@ -10,7 +10,8 @@ import PieChart from '../../components/PieChart.vue';
 var endDate = new Date('2023-03-27');
 var orientation= 'horizontal';
 
-
+const adminStore = useAdminStore();
+const { stats, InteractionDates } = storeToRefs(adminStore);
 
 
 </script>
