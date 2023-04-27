f06c6b816912ae5ea05a417fa172c346f17d0bc3
AlexOS <82172155+alexandredesbos@users.noreply.github.com>
2023-04-27 21:52:13 +0200

    Update UserHelp.vue

--- front/src/views/user/UserHelp.vue
+++ front/src/views/user/UserHelp.vue
@@ -17,24 +17,24 @@ toHelpStore.getDataToHelp(route.params.id);
 </script>
 
 <template>
+    <div class="container-help-user">
+        <h1 class="titre">Vous êtes le profil parfait pour aider {{ userToHelp.user.profile[0].pseudo }} !</h1>
 
-    <h1 class="titre">Vous êtes le profil parfait pour aider {{ userToHelp.user.profile[0].pseudo }} !</h1>
-
-    <!-- Affiche le profil de l'utilisateur qui a besoin d'aide -->
-    <div class="container-profile-">
-        <div class="container-card"> <UserCard2 :nom=userToHelp.user.profile[0].pseudo :techno=userToHelp.user.profile[1] :avatar=userToHelp.user.profile[0].avatar  :reco=userToHelp.user.profile[1][0].techno :key=userToHelp :lastInteract=userToHelp.user.profile[0].lastInteraction /> </div>
-    </div>
-
-    <h2>Voici les questions que vous pouvez aider à résoudre :</h2>
-
-    <!-- affiche une liste de questions avec le nom de la question et un bouton pour acceder à l'url de la quesiton -->
-    <ul>
-        <div class="list-row" v-for="question in userToHelp.user.questions">
-            <li>{{ question.title }}</li>
-            <a :href="question.urlQuestion" target="_blank" class="question"><button class="btn btn-primary">Accéder à la question</button></a>
+        <!-- Affiche le profil de l'utilisateur qui a besoin d'aide -->
+        <div class="container-profile-">
+            <div class="container-card"> <UserCard2 :nom=userToHelp.user.profile[0].pseudo :techno=userToHelp.user.profile[1] :avatar=userToHelp.user.profile[0].avatar  :reco=userToHelp.user.profile[1][0].techno :key=userToHelp :lastInteract=userToHelp.user.profile[0].lastInteraction /> </div>
         </div>
-    </ul>
 
+        <h2>Voici les questions que vous pouvez aider à résoudre :</h2>
+
+        <!-- affiche une liste de questions avec le nom de la question et un bouton pour acceder à l'url de la quesiton -->
+        <ul>
+            <div class="list-row" v-for="question in userToHelp.user.questions">
+                <li>{{ question.title }}</li>
+                <a :href="question.urlQuestion" target="_blank" class="question"><button class="btn btn-primary">Accéder à la question</button></a>
+            </div>
+        </ul>
+    </div>
     
 
 </template>
@@ -54,6 +54,11 @@ toHelpStore.getDataToHelp(route.params.id);
     align-items: center;
     margin-bottom: 20px;
     
+}
+
+
+.container-help-user{
+    animation: reveal 1s cubic-bezier(.17,.67,.4,1.02); 
 }
 
 
