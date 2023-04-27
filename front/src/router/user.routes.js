dc8108a91e19fe84e94dda8ee860be3bf7e10d56
BaptisteGriva <75640057+ThxZ3US@users.noreply.github.com>
2023-04-27 16:14:03 +0200

    maj

--- front/src/router/user.routes.js
+++ front/src/router/user.routes.js
@@ -1,14 +1,13 @@
-import { Layout, Home, Statistics, UserPreferences } from '@/views/user';
-
+import { Layout, Home, Statistics, UserPreferences, Help } from "@/views/user";
 
 export default {
-    path: '/',
-    component: Layout,
-    children: [
-        { path: '/overview', component: Home },
-        { path: '/statistics', component: Statistics },
-        { path: '/help/:id', component: Help },
-        { path: '/preferences', component: UserPreferences},
-        { path: '', redirect: '/overview'}
-    ]
+  path: "/",
+  component: Layout,
+  children: [
+    { path: "/overview", component: Home },
+    { path: "/statistics", component: Statistics },
+    { path: "/help/:id", component: Help },
+    { path: "/preferences", component: UserPreferences },
+    { path: "", redirect: "/overview" },
+  ],
 };
